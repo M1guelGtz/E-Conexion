851ab348b6775554d2c12d241bbdfbe4e2974c55
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-11-15 21:14:58 -0600

    Add: Variable de entorno para la api, metodo get de publicaciones conectado ala API

---
+++ src/app/Interfaces/publicacion.ts
@@ -0,0 +1,9 @@
+export interface Publicacion {
+    descripcion: string
+    fecha: string 
+    id_publicaciones: number
+    id_publicaciones_usuario: number
+    imagen: string | null
+    nombre_usuario: string,
+    titulo: string
+}
