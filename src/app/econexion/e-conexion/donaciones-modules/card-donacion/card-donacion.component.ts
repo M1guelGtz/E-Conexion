0c2e0d1462872412936729ff243e5f06eeb98373
carlos22101 <233429@ids.upchiapas.edu.mx>
2024-11-20 21:13:06 -0600

    Donaciones CRUD

--- src/app/econexion/e-conexion/donaciones-modules/card-donacion/card-donacion.component.ts
+++ src/app/econexion/e-conexion/donaciones-modules/card-donacion/card-donacion.component.ts
@@ -1,10 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Donacion } from '../../../../Interfaces/donacion';
+import { DonacionesService } from '../../../../servicios/donacion.service';
+import { EventosService } from '../../../../servicios/eventos.service';
+import { Eventos } from '../../../../Interfaces/eventos';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-card-donacion',
   templateUrl: './card-donacion.component.html',
   styleUrls: ['./card-donacion.component.css']
 })
-export class CardDonacionComponent {
+export class CardDonacionComponent implements OnInit{
   @Input() donacion!: Donacion;
+  evento! : Eventos;
+  modal : boolean = false;
+
+  constructor ( public _evento_service : EventosService, private router: Router) {  }
+  
+  ngOnInit(): void {
+    this. _evento_service.obtenerEventoPorId(this.donacion.id_evento).subscribe( ( evento ) => {
+      this.evento = evento
+      
+    } )
+  }
+  handleClickExpandEvent(){
+    this.modal = true
+  }
+  handleClickCloseModal( ) {
+    this.modal = false
+  }
+
+  actualizarDonacion(idDonacion : number): void{
+    this.router.navigate([`red/Donaciones/donacionesform/${idDonacion}`]);
+  }
+
 }
+
+
