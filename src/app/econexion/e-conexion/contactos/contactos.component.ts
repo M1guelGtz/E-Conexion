c64224b0b9b7c97d88149819d1ebc438b03f6326
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-11-23 13:34:51 -0600

    marge resolve

--- src/app/econexion/e-conexion/contactos/contactos.component.ts
+++ src/app/econexion/e-conexion/contactos/contactos.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Contacto } from '../../../Interfaces/contacto';
 import { ContactosService } from '../../../servicios/contactos.service';
-import { Component, OnInit } from '@angular/core';
-import { Contacto } from '../../../Interfaces/contacto';
-import { ContactosService } from '../../../servicios/contactos.service';
 
 @Component({
   selector: 'app-contactos',
@@ -28,16 +25,5 @@ export class ContactosComponent implements OnInit {
 
 
 } 
-export class ContactosComponent implements OnInit {
-  constructor ( private _servicio_contactos : ContactosService ) {  }
-  contactos : Contacto[] = []
-  id_user = 1;
-  ngOnInit(): void {
-      this._servicio_contactos. getMyContactosById(this.id_user).subscribe( list_contacts => {
-        this.contactos = list_contacts
-      })
-  }
-  
 
 
-} 
